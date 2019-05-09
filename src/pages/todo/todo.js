8f36755c3a5494e9ad6c24f965f7b712e4d4d437
abdoulk9 <abdoulkadre@yahoo.fr>
2019-05-09 14:38:22 +0200

    affichage de la liste

--- src/pages/todo/todo.js
+++ src/pages/todo/todo.js
@@ -1,18 +1,69 @@
 import React from 'react';
+import Axios from 'axios';
 
 const URL = 'https://jsonplaceholder.typicode.com/todos';
 
+function TodoList(props) {
 
-
-function Todo() {
-
+    const tableRows = props.data.map(
+        (task) => {
+            return (
+                <tr>
+                    <td>{task.title}</td>
+                    <td>{task.complited ? 'OUI' : 'NON'}</td>
+                </tr>
+            );
+        }
+    );
+   
 
     return (
-        <div>
-            <h1>Liste des tâches</h1>
-        </div>
+        <table>
+            <thead>
+                <tr>
+                    <th>tâche</th>
+                    <th>Fait</th>
+                </tr>
+            </thead>
+            <tbody>
+                {tableRows}
+            </tbody>
+        </table>
     );
 }
 
+
+
+class Todo extends React.Component {
+    
+    state = {
+        todoList: []
+    }
+
+
+    //Apple asynchrone avec la biblio axios
+    constructor() {
+        super();
+
+        Axios.get(URL).then(
+            (response) => {
+                console.log(response.data);
+                //recuperation des données dans un tableau
+                //Avec this.setState
+                this.setState({ todoList: response.data });
+            }
+        );
+    }
+    render() {
+        return (
+            <div>
+                <h1>Liste des tâches</h1>
+
+                <TodoList data={this.state.todoList} />
+            </div>
+        );
+    }
+
+}
 export default Todo
 
