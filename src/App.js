fb47453657447e3ee2caf501e9fd285e1eade904
abdoulk9 <abdoulkadre@yahoo.fr>
2019-05-09 11:13:27 +0200

    construction component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BowserRouter as router, Switch, Route, Link} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Router>
+          <div>
+            <ul>
+              <li>
+                <Link to="/todo">Liste des tâches</Link>
+              </li>
+            </ul>
+          </div>
+          <switch>
+            <Route exactly pattern="/todo" component={} />
+            <Route component={}/>
+          </switch>
+
+        </Router>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ src/pages/todo/todo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+function Todo(){
+
+      
+    return(
+        <div>
+            <h1>Liste des tâches</h1>
+        </div>
+    );
+}
+
