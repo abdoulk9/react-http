2fd6326f08017b1406942fbaa280315f24f1070e
abdoulk9 <abdoulkadre@yahoo.fr>
2019-05-10 11:39:40 +0200

    pagination

--- src/pages/todo/todo.js
+++ src/pages/todo/todo.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Axios from 'axios';
 
+import queryString from 'query-string';
+
 const URL = 'https://jsonplaceholder.typicode.com/todos';
 
 function TodoList(props) {
@@ -9,7 +11,7 @@ function TodoList(props) {
         (task) => {
             return (
                 // identification unique de chaque tr avec id
-                <tr key={task.id}>   
+                <tr key={task.id}>
                     <td>{task.id}</td>
                     <td>{task.title}</td>
                     <td>{task.complited ? 'OUI' : 'NON'}</td>
@@ -17,7 +19,7 @@ function TodoList(props) {
             );
         }
     );
-   
+
 
     return (
         <table className="table table-bordered table-striped">
@@ -43,10 +45,17 @@ class Todo extends React.Component {
         todoList: []
     }
 
+    //debut de page
+    correntPage = 1;
+
+    //Nbre d'element par page
+    nberOfItemPerPage = 10;
+
+    nbreOfPages;
 
     //Apple asynchrone avec la biblio axios
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
 
         Axios.get(URL).then(
             (response) => {
@@ -54,16 +63,23 @@ class Todo extends React.Component {
                 //recuperation des données dans un tableau
                 //Avec this.setState
                 this.setState({ todoList: response.data });
+                this.nbreOfPages = Math.ceil(response.data.length /
+                    this.nberOfItemPerPage);
             }
         );
-
+        //this.props.location.search: Permet de recuperer la clef et la valeur de notre 
+        // chaine de catarcter traiter par queryString.parse(); exemple: /page=5(clef=page/ valeur=5)
+        let values = queryString.parse(this.props.location.search);
+        this.currentPage = values.pages || 1;
     }
     render() {
         return (
             <div>
                 <h1>Liste des tâches</h1>
 
-                <TodoList data={this.state.todoList} />
+                <TodoList data={this.state.todoList.slice(
+                    (this.currentPage - 1) * this.nberOfItemPerPage,
+                    this.currentPage * this.nberOfItemPerPage)} />
             </div>
         );
     }
